67e82da9d07639e65e641170ba84e103e5a91d5f
Luis Ignacio Chapa <luisignaciochapa@gmail.com>
2022-11-02 01:34:41 -0300

    Re-Entrega

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+let precioMilanesa = 5
+let precioBife = 6
+let precioFideos = 4
+let precioBebida = 3
+let resultado = 0
+
+function suma (a,b) {
+    resultado = a+b
+}
+
 let nombreCliente = prompt(`¡Hola!, como te llamas?`)
 
     while (nombreCliente != Number) {
@@ -5,23 +15,29 @@ let nombreCliente = prompt(`¡Hola!, como te llamas?`)
         
         (ingrese 1, 2 o 3 segun corresponda)
 
-        1) Milanesa con puré
-        2) Bife con ensalada
-        3) Fideos con salsa`)
+        1) Milanesa con puré + bebida
+        2) Bife con ensalada + bebida
+        3) Fideos con salsa + bebida`)
 
         if (pedido == 1) {
+            suma(precioMilanesa,precioBebida)
+            alert(`El total de la factura es $` + resultado)
             alert(`Tu pedido se realizó con éxito`)
             console.log(pedido)
             break
         }
 
         if (pedido == 2) {
+            suma(precioBife,precioBebida)
+            alert(`El total de la factura es $` + resultado)
             alert(`Tu pedido se realizó con éxito`)
             console.log(pedido)
             break
         }
 
         if (pedido == 3) {
+            suma(precioFideos,precioBebida)
+            alert(`El total de la factura es $` + resultado)
             alert(`Tu pedido se realizó con éxito`)
             console.log(pedido)
             break
